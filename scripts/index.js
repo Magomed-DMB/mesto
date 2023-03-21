a3e5e29ab33bd15f104356ff02db7450d9ff21d5
DMB Magomed <06mohmad1416@gmail.com>
2023-03-21 23:07:41 +0300

    7 спринт

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
-import Card from '/Card.js';
-import FormValidator from '/FormValidator.js';
+import Card from 'mesto/scripts/Card.js';
+import FormValidator from 'mesto/scripts/FormValidator.js';
 
 const popupProfileOpen = document.querySelector('.popup_profile');
 const popupElementOpen = document.querySelector('.popup_element');
